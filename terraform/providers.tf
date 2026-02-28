7f2cf5b37fb55110eb8d5099c4f556ab03010aa5
Omarahorrar5 <omarahorar@gmail.com>
2026-02-28 01:10:13 +0000

    adding s3 backend for terraform state

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket  = "omar-terraform-state-eks"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
